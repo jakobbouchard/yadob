75d19b66f6ea9b509218484dea5e5491639aeaa4
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-03 11:49:16 -0400

    Overhauled ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,23 @@
 module.exports = (message) => {
-  message.reply("Pong!").catch(e => console.log(e));
+  let pingEmbed = {
+    color: 0x18bc9c,
+    title: 'Ping',
+    description: 'Pinging...',
+    author: {
+      name: 'YADOB',
+      icon_url: 'https://i.imgur.com/wSTFkRM.png',
+      url: 'https://yadob.jakobbouchard.dev',
+    }
+  };
+  message.channel.send({ embed: pingEmbed })
+    .then((msg) => {
+      let newPingEmbed = {
+        color: pingEmbed.color,
+        title: pingEmbed.title,
+        description: `The latency is ${Date.now() - msg.createdTimestamp}ms.`,
+        author: pingEmbed.author
+      };
+      msg.edit({ embed: newPingEmbed })
+    })
+    .catch(e => console.log(e))
 }
