3023f5d15ba184444533325011c67dffa5fc0884
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-03 11:30:45 -0400

    Fix nickname command so that clearing works

--- commands/nickname.js
+++ commands/nickname.js
@@ -9,18 +9,18 @@ module.exports = (message) => {
       url: 'https://yadob.jakobbouchard.dev',
     }
   };
-  if (params.length <= 32) {
-    message.guild.me.setNickname(params)
-      .then(nicknameEmbed.description = `Nickname changed to ${params}!`)
-      .catch(console.error)
+  if (!params.length) {
+    nicknameEmbed.description = `Please enter a nickname.`;
+  } else if (params.length > 32) {
+    nicknameEmbed.description = `A nickname must be between 1 and 32 characters.`;
   } else if (params == 'clear') {
     message.guild.me.setNickname()
       .then(nicknameEmbed.description = `Nickname cleared!`)
       .catch(console.error)
-  } else if (params.length > 32) {
-      nicknameEmbed.description = `A nickname must be between 1 and 32 characters.`;
   } else {
-    nicknameEmbed.description = `Please enter a nickname or \`clear\` to remove the nickname.`;
+    message.guild.me.setNickname(params)
+      .then(nicknameEmbed.description = `Nickname changed to ${params}!`)
+      .catch(console.error)
   };
 
   message.channel.send({ embed: nicknameEmbed }).catch(e => console.log(e));
