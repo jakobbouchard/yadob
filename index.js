e05c9ed75a884d22cdab9437a863995f0f578c04
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-03 08:42:36 -0400

    Change bot to client, for a bit more standard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 let {prefix, token} = require('./config.json');
 const path = require('path');
 const fs = require('fs');
@@ -10,26 +10,29 @@ fs.readdir(directoryPath, function (err, files) {
   if (err) {
     return console.log('Unable to scan directory: ' + err);
   }
+  console.log(`Loading a total of ${files.length} commands.`);
   files.forEach(function (file) {
     commands[file.replace('.js', '')] = require('./commands/' + file);
+    console.log(`Command Loaded! ${file}`);
   });
 });
 
-bot.login(token);
+client.login(token);
 
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', message => {
+client.on('message', message => {
   const commandList = Object.keys(commands);
   const normalizedMessage = message.content.toLowerCase();
+  console.log(normalizedMessage)
 
   if (message.author.bot) return;
   if (!normalizedMessage.startsWith(prefix)) return;
   commandList.forEach(function(item) {
     if (normalizedMessage.startsWith(prefix + item)) {
-      commands[item](message, commandList);
+      commands[item](message, client, commandList);
     }
   })
 });
