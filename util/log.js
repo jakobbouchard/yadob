b0232be474b71abfae7a31d85d8ce56273b5873f
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-03 23:38:13 -0400

    Fix and revamp timestamps

--- util/log.js
+++ util/log.js
@@ -1,5 +1,19 @@
-const date = new Date()
-const timestamp = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`
+const getTimestamp = () => {
+  const date = new Date()
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const milliseconds = date.getMilliseconds();
+  return '[' +
+         ((hour < 10) ? '0' + hour: hour) +
+         ':' +
+         ((minutes < 10) ? '0' + minutes: minutes) +
+         ':' +
+         ((seconds < 10) ? '0' + seconds: seconds) +
+         '.' +
+         ('00' + milliseconds).slice(-3) +
+         '] ';
+}
 const colours = {
   reset: "\x1b[0m",
   info: "\x1b[94m",
@@ -9,14 +23,14 @@ const colours = {
 }
 
 exports.info = message => {
-  console.log(`${timestamp} - ${colours.info}[LOG]${colours.reset} %s`, message)
+  console.log(`${getTimestamp()}${colours.info}[LOG]${colours.reset} %s`, message)
 }
 exports.success = message => {
-  console.log(`${timestamp} - ${colours.success}[OK]${colours.reset} %s`, message)
+  console.log(`${getTimestamp()}${colours.success}[OK]${colours.reset} %s`, message)
 }
 exports.warn = message => {
-  console.log(`${timestamp} - ${colours.warn}[WARN]${colours.reset} %s`, message)
+  console.log(`${getTimestamp()}${colours.warn}[WARN]${colours.reset} %s`, message)
 }
 exports.error = message => {
-  console.log(`${timestamp} - ${colours.error}[ERROR]${colours.reset} %s`, message)
+  console.log(`${getTimestamp()}${colours.error}[ERROR]${colours.reset} %s`, message)
 }
