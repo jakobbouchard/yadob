787a16d7cb66b9f2a6411a4675b3d9e5d6446fa3
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-05 00:04:53 -0400

    Remove alias support and add semicolons

--- commands/_loader.js
+++ commands/_loader.js
@@ -4,30 +4,32 @@ const log = require(`../util/log.js`);
 
 module.exports = client => {
   client.commands = new Collection();
-  client.aliases = new Collection();
 
   fileSystem.readdir(__dirname, (err, files) => {
     if (err) {
       return log.error(`Unable to scan directory: ${err}`);
     }
 
-    const commandFiles = files.filter(file => (file.split(`.`).pop() === `js` && file != `_loader.js`))
+    const commandFiles = files.filter(file => (file.split(`.`).pop() === `js` && file != `_loader.js`));
     if (commandFiles.length <= 0) {
-      return log.error(`No commands were found.`)
+      return log.error(`No commands were found.`);
     }
 
     commandFiles.forEach(async file => {
       try {
-        const command = await require(`${__dirname}\\${file}`)
+        const command = await require(`${__dirname}\\${file}`);
         if (command.settings.enabled) {
-          await client.commands.set(command.help.name, command)
-          log.success(`Command loaded - ${command.help.name}`)
+          await client.commands.set(command.help.name, command);
+          log.success(`Command loaded - ${command.help.name}`);
         } else {
-          log.warn(`Command is disabled - ${command.help.name}`)
+          log.warn(`Command is disabled - ${command.help.name}`);
+        }
+        if (command.preload) {
+          command.preload(client);
         }
       } catch (err) {
-        log.error(`Couldn't load ${file}: ${err}`)
+        log.error(`Couldn't load ${file}: ${err}`);
       }
-    })
-  })
+    });
+  });
 }
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Discord = require(`discord.js`)
-const client = new Discord.Client({ disableEveryone: true })
-const { prefix, token } = require(`./config.js`)
-const log = require(`./util/log.js`)
-require(`./commands/_loader.js`)(client)
+const Discord = require(`discord.js`);
+const client = new Discord.Client({ disableEveryone: true });
+const { prefix, token } = require(`./config.js`);
+const log = require(`./util/log.js`);
+require(`./commands/_loader.js`)(client);
 
 client.login(token)
   .then(() => log.success(`Logged in successfully as ${client.user.tag}!`))
-  .catch(err => log.error(err))
+  .catch(err => log.error(err));
 
 client.on(`ready`, () => {
-  client.user.setActivity(`${prefix}help`, { type: `LISTENING` })
-})
+  client.user.setActivity(`${prefix}help`, { type: `LISTENING` });
+});
 
-client.on(`error`, err => log.error(err))
+client.on(`error`, err => log.error(err));
 
-client.on(`message`, message => require(`./messageHandler.js`)(client, message))
+client.on(`message`, message => require(`./messageHandler.js`)(client, message));
