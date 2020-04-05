cbe25c89c92a286f642b8de8422936385827d297
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-04 22:55:41 -0400

    Optimize the code a bit and better error logging

--- commands/_loader.js
+++ commands/_loader.js
@@ -1,28 +1,28 @@
-const Discord = require(`discord.js`);
+const { Collection } = require(`discord.js`);
 const fileSystem = require(`fs`);
 const log = require(`../util/log.js`);
 
 module.exports = client => {
-  client.commands = new Discord.Collection();
-  client.aliases = new Discord.Collection();
-  
+  client.commands = new Collection();
+  client.aliases = new Collection();
+
   fileSystem.readdir(__dirname, (err, files) => {
     if (err) {
       return log.error(`Unable to scan directory: ${err}`);
     }
 
     const commandFiles = files.filter(file => (file.split(`.`).pop() === `js` && file != `_loader.js`))
-    if(commandFiles.length <= 0) {
+    if (commandFiles.length <= 0) {
       return log.error(`No commands were found.`)
     }
 
     commandFiles.forEach(async file => {
       try {
-        const command = require(`${__dirname}\\${file}`)
+        const command = await require(`${__dirname}\\${file}`)
         await client.commands.set(command.help.name, command)
         log.success(`Command loaded - ${command.help.name}`)
       } catch (err) {
-        log.error(`Couldn't load ${file}`)
+        log.error(`Couldn't load ${file}: ${err}`)
       }
     })
   })
