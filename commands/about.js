533d963a82407e9863ab4d2d86932b84d0765972
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-04 22:37:16 -0400

    New command loader

---
+++ commands/about.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+
+exports.run = (client, message, args) => {
+  const embed = new MessageEmbed()
+    .setColor(0xFFFF00)
+    .addField('About The Bot', `YADOB is a bot created and maintained by Jakob Bouchard. It is written with Discord.js.`)
+    .setFooter(`© YADOB by Jakob Bouchard`);
+
+  message.channel.send({ embed }).catch(err => log.error(err));
+};
+
+exports.settings = {
+  enabled: true,
+  preload: false,
+  useInDM: true,
+  aliases: []
+};
+
+exports.help = {
+  name: 'about',
+  description: 'About the bot.',
+  usage: 'about'
+};
--- messageHandler.js
+++ messageHandler.js
@@ -1,4 +1,5 @@
 const {prefix} = require('./config.js');
+const log = require(`./util/log.js`);
 
 module.exports = message => {
   const normalizedMessage = message.content.toLowerCase();
@@ -9,7 +10,7 @@ module.exports = message => {
   if (!normalizedMessage.startsWith(prefix)) return; 
 
   let command = normalizedMessage.split(' ')[0].slice(prefix.length);
-  let params = normalizedMessage.split(' ').slice(1);
+  let args = normalizedMessage.split(' ').slice(1);
   let cmd;
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
@@ -17,9 +18,7 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
-    cmd(client, message, params);
-    // cmd.run(client, message, params);
-    // console.log("Command: /" + cmd.help.name)
-    // console.log("Guild: " + message.guild.name)
+    cmd.run(client, message, args);
+    log.info(`[${message.guild.name}]${message.member.user.tag} Command: ${cmd.help.name}`)
   }
 };
