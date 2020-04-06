143747c2a82e0da4c41b8ec205bf1e6deda13bec
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-06 13:17:54 -0400

    Fix reload command using anidiots.guide. Closes #11

--- commands/reload.js
+++ commands/reload.js
@@ -8,15 +8,17 @@ exports.run = async (client, message, args) => {
     .setDescription(`Commands reloaded!`)
     .setAuthor(`YADOB`, `https://i.imgur.com/wSTFkRM.png`, `https://yadob.jakobbouchard.dev`);
 
+  await client.commands.each(command => {
+    delete require.cache[require.resolve(`./${command.info.name}.js`)];
+  });
   await client.commands.clear();
-  delete require.cache[require.resolve(`./_loader.js`)];
   await require(`./_loader.js`)(client);
 
   message.channel.send(embed).catch(err => log.error(err));
 };
 
 exports.settings = {
-  enabled: false,
+  enabled: true,
   dmUse: true
 };
 
