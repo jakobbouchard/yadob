2fbb2feaf16cefcbb3cd0ade3ae1431a01fd17d8
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-06 13:11:33 -0400

    Fix ping command using eslachance's bot example. Closes #5

--- commands/ping.js
+++ commands/ping.js
@@ -4,13 +4,13 @@ const log = require(`../util/log.js`);
 exports.run = async (client, message, args) => {
   const embed = new MessageEmbed()
     .setColor(`#18bc9c`)
-    .setTitle(`Ping`)
+    .setTitle(`Pong!`)
     .setDescription(`Pinging...`)
     .setAuthor(`YADOB`, `https://i.imgur.com/wSTFkRM.png`, `https://yadob.jakobbouchard.dev`);
 
   message.channel.send(embed)
     .then(msg => {
-      embed.setDescription(`The latency is ${Date.now() - msg.createdTimestamp}ms.`);
+      embed.setDescription(`The latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
       msg.edit(embed);
     })
     .catch(err => log.error(err));
