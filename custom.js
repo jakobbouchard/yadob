074fbaaeb84ceeef143025de220e365648ca805c
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-04-07 00:51:06 -0400

    Change logo links and change footer info

--- custom.js
+++ custom.js
@@ -2,19 +2,20 @@ exports.botInfo =
 {
   // Used in the embed author section. Will maybe change the avatar and username at some point.
   name: `YADOB`,
-  logo: `assets/images/logo-32x32.png`,
-  thumbnail: `assets/images/logo-330x330.png`,
+  logo: `https://yadob.jakobbouchard.dev/assets/images/logo-32x32.png`,
+  // Used in the bot's about and help commands. Can also be used in other commands if wanted.
+  thumbnail: `https://yadob.jakobbouchard.dev/assets/images/logo-330x330.png`,
   // This website will be linked on the author in embeds, as well as other places where the bot is mentioned.
   website: `https://yadob.jakobbouchard.dev`
 }
 
 exports.embed = {
   // Will the author section appear in the embed?
-  useAuthor: true,
+  useAuthor: false,
   // Will the footer section appear in the embed?
   useFooter: true,
   // The color on the side of the embed.
   color: `#A070A1`,
   // The text appearing in the footer.
-  footer: `Copyright © ${this.botInfo.name} ${new Date().getFullYear()}`
+  footer: `Maintained by Jakob Bouchard - https://yadob.jakobbouchard.dev`
 }
